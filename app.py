e45687df232ec46abe78f0af4857178899aaeec7
Madeline Warner <madelinewarner16@gmail.com>
2024-04-07 22:08:36 -0500

    added templates, basics of two templates

--- app.py
+++ app.py
@@ -1,3 +1,42 @@
+# -*- coding: utf-8 -*-
+
+import flask
+from flask import request, render_template
+from flask.typing import ResponseReturnValue
+
+app = flask.Flask(__name__)
+
+
+@app.route("/scheduler/twelve_teams")
+def get_input_twelve_team():
+    return render_template("src/templates/twelve_team_input.html")
+
+
+@app.route("/scheduler/schedule_twelve_teams", methods=["POST"])
+def create_schedule():
+    form_input_data = {
+        "division_one": {
+            "team_one": request.form["div_one_team_one"],
+            "team_two": request.form["div_one_team_two"],
+            "team_three": request.form["div_one_team_three"],
+            "team_four": request.form["div_one_team_four"]
+        },
+        "division_two": {
+            "team_one": request.form["div_one_team_one"],
+            "team_two": request.form["div_one_team_two"],
+            "team_three": request.form["div_one_team_three"],
+            "team_four": request.form["div_one_team_four"]
+        },
+        "divison_three": {
+            "team_one": request.form["div_one_team_one"],
+            "team_two": request.form["div_one_team_two"],
+            "team_three": request.form["div_one_team_three"],
+            "team_four": request.form["div_one_team_four"]
+        }
+    }
+    # TODO: create a schedule
+    return render_template("src/templates/twelve_team_schedule.html")
+
 
 
 #TODO: Make this into a Flask app with an exposed endpoint that takes in the teams per division and returns the schedule
